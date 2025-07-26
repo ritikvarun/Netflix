27367a9a0c9a1f948b54d785ead0c93fb19bfeda
Ritik varun <132301755+ritikvarun@users.noreply.github.com>
2025-07-26 13:01:28 +0530

    /r

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import userRoute from "./routes/userRoute.js";
 import cors from "cors";
 import path from "path";
+import { error } from "console";
 databaseConnection();
 dotenv.config({
   path: ".env",
@@ -25,7 +26,12 @@ app.use(cors(corsOption));
 
 //API
 app.use("/api/v1/user", userRoute);
-
+app.get("/",(req,res)=>{
+  res.send({
+    activeStatus:true,
+    error:false  
+  })
+})
 app.use(express.static(path.join(_dirname,"/frontend/dist")));
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
